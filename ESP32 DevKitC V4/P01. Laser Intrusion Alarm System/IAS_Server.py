82bac79fa339f00f3947b5377ce2fd5e1c3e5679
taejin-seong <sungtj0130@naver.com>
2022-05-20 03:14:05 +0900

    Implementation of a simple laser intrusion alarm system

---
+++ ESP32 DevKitC V4/P01. Laser Intrusion Alarm System/IAS_Server.py
@@ -0,0 +1,21 @@
+import socket
+
+s = socket.socket()
+
+s.bind(('server ip',server port)) #Input IP, PORT
+s.listen(0)
+
+while True:
+    client, aaddr = s.accept()
+    while True:
+        content = client.recv(31)
+
+        if len(content) == 0:
+            break
+
+        else:
+            # default utf-8
+            decoded_content = content.decode(encoding='utf-8')
+       #     decoded_content.strip("♪")
+            print(decoded_content)
+    client.close()
