93ac59ba305a4def5078b5a67b528025aa692274
taejin-seong <sungtj0130@naver.com>
2022-05-23 02:44:00 +0900

    TCP/IP BSD Socket Test

---
+++ ESP32 DevKitC V4/T03. Python TCP Server/Server_Test.py
@@ -0,0 +1,21 @@
+import socket
+
+s = socket.socket()
+
+s.bind(('?',?)) #Input IP, PORT
+s.listen(0)
+
+while True:
+    client, aaddr = s.accept()
+    while True:
+        content = client.recv(31)
+
+        if len(content) == 0:
+            break
+
+        else:
+            # default utf-8
+            decoded_content = content.decode(encoding='utf-8')
+            print(decoded_content)
+    print("Closing connection")
+    client.close()
