9655e94f3b947a5f3c75dc09b13ebae640de7be5
taejin-seong <sungtj0130@naver.com>
2022-05-23 18:14:08 +0900

    OTA Web Updater DFU Src.bin init

--- ESP32 DevKitC V4/build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ ESP32 DevKitC V4/build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -1,78 +1,186 @@
-# 1 "d:\\ESP32\\ESP32 DevKitC V4\\T05. Basic OTA DFU Src\\Basic_OTA_DFU_Src_Test.ino"
-# 2 "d:\\ESP32\\ESP32 DevKitC V4\\T05. Basic OTA DFU Src\\Basic_OTA_DFU_Src_Test.ino" 2
-# 3 "d:\\ESP32\\ESP32 DevKitC V4\\T05. Basic OTA DFU Src\\Basic_OTA_DFU_Src_Test.ino" 2
-# 4 "d:\\ESP32\\ESP32 DevKitC V4\\T05. Basic OTA DFU Src\\Basic_OTA_DFU_Src_Test.ino" 2
-# 5 "d:\\ESP32\\ESP32 DevKitC V4\\T05. Basic OTA DFU Src\\Basic_OTA_DFU_Src_Test.ino" 2
+# 1 "d:\\ESP32\\ESP32 DevKitC V4\\T06. OTA Web Updater DFU Src\\OTA_Web_Updater_DFU_Src_Test.ino"
+# 2 "d:\\ESP32\\ESP32 DevKitC V4\\T06. OTA Web Updater DFU Src\\OTA_Web_Updater_DFU_Src_Test.ino" 2
+# 3 "d:\\ESP32\\ESP32 DevKitC V4\\T06. OTA Web Updater DFU Src\\OTA_Web_Updater_DFU_Src_Test.ino" 2
+# 4 "d:\\ESP32\\ESP32 DevKitC V4\\T06. OTA Web Updater DFU Src\\OTA_Web_Updater_DFU_Src_Test.ino" 2
+# 5 "d:\\ESP32\\ESP32 DevKitC V4\\T06. OTA Web Updater DFU Src\\OTA_Web_Updater_DFU_Src_Test.ino" 2
+# 6 "d:\\ESP32\\ESP32 DevKitC V4\\T06. OTA Web Updater DFU Src\\OTA_Web_Updater_DFU_Src_Test.ino" 2
 
 /* LED */
 
 int ledState = 0x0;
 
+const char* host = "esp32";
 const char* ssid = "//";
 const char* password = "//";
 
-void setup() {
+WebServer server(80);
+
+/*
+
+ * Login page
+
+ */
+# 21 "d:\\ESP32\\ESP32 DevKitC V4\\T06. OTA Web Updater DFU Src\\OTA_Web_Updater_DFU_Src_Test.ino"
+const char* loginIndex =
+ "<form name='loginForm'>"
+    "<table width='20%' bgcolor='A09F9F' align='center'>"
+        "<tr>"
+            "<td colspan=2>"
+                "<center><font size=4><b>ESP32 Login Page</b></font></center>"
+                "<br>"
+            "</td>"
+            "<br>"
+            "<br>"
+        "</tr>"
+        "<tr>"
+             "<td>Username:</td>"
+             "<td><input type='text' size=25 name='userid'><br></td>"
+        "</tr>"
+        "<br>"
+        "<br>"
+        "<tr>"
+            "<td>Password:</td>"
+            "<td><input type='Password' size=25 name='pwd'><br></td>"
+            "<br>"
+            "<br>"
+        "</tr>"
+        "<tr>"
+            "<td><input type='submit' onclick='check(this.form)' value='Login'></td>"
+        "</tr>"
+    "</table>"
+"</form>"
+"<script>"
+    "function check(form)"
+    "{"
+    "if(form.userid.value=='admin' && form.pwd.value=='admin')" // initial username, password = admin, admin 
+    "{"
+    "window.open('/serverIndex')"
+    "}"
+    "else"
+    "{"
+    " alert('Error Password or Username')/*displays error message*/"
+    "}"
+    "}"
+"</script>";
+
+/*
+
+ * Server Index Page
+
+ */
+# 67 "d:\\ESP32\\ESP32 DevKitC V4\\T06. OTA Web Updater DFU Src\\OTA_Web_Updater_DFU_Src_Test.ino"
+const char* serverIndex =
+"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
+"<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
+   "<input type='file' name='update'>"
+        "<input type='submit' value='Update'>"
+    "</form>"
+ "<div id='prg'>progress: 0%</div>"
+ "<script>"
+  "$('form').submit(function(e){"
+  "e.preventDefault();"
+  "var form = $('#upload_form')[0];"
+  "var data = new FormData(form);"
+  " $.ajax({"
+  "url: '/update',"
+  "type: 'POST',"
+  "data: data,"
+  "contentType: false,"
+  "processData:false,"
+  "xhr: function() {"
+  "var xhr = new window.XMLHttpRequest();"
+  "xhr.upload.addEventListener('progress', function(evt) {"
+  "if (evt.lengthComputable) {"
+  "var per = evt.loaded / evt.total;"
+  "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
+  "}"
+  "}, false);"
+  "return xhr;"
+  "},"
+  "success:function(d, s) {"
+  "console.log('success!')"
+ "},"
+ "error: function (a, b, c) {"
+ "}"
+ "});"
+ "});"
+ "</script>";
+
+/*
+
+ * setup function
+
+ */
+# 107 "d:\\ESP32\\ESP32 DevKitC V4\\T06. OTA Web Updater DFU Src\\OTA_Web_Updater_DFU_Src_Test.ino"
+void setup(void) {
   Serial.begin(115200);
-  Serial.println("Booting");
-  WiFi.mode(WIFI_MODE_STA);
+
+  // Connect to WiFi network
   WiFi.begin(ssid, password);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
-
-  // Port defaults to 3232
-  // ArduinoOTA.setPort(3232);
-
-  // Hostname defaults to esp3232-[MAC]
-  // ArduinoOTA.setHostname("myesp32");
-
-  // No authentication by default
-  // ArduinoOTA.setPassword("admin");
-
-  // Password can be set with it's md5 value as well
-  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
-
-  ArduinoOTA
-    .onStart([]() {
-      String type;
-      if (ArduinoOTA.getCommand() == 0)
-        type = "sketch";
-      else // U_SPIFFS
-        type = "filesystem";
-
-      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-      Serial.println("Start updating " + type);
-    })
-    .onEnd([]() {
-      Serial.println("\nEnd");
-    })
-    .onProgress([](unsigned int progress, unsigned int total) {
-      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    })
-    .onError([](ota_error_t error) {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    });
-
-  ArduinoOTA.begin();
-
-  Serial.println("Ready");
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  /* LED */
+  /*use mdns for host name resolution*/
+  if (!MDNS.begin(host)) { //http://esp32.local
+    Serial.println("Error setting up MDNS responder!");
+    while (1) {
+      delay(1000);
+    }
+  }
+  Serial.println("mDNS responder started");
+  /*return index page which is stored in serverIndex */
+  server.on("/", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", loginIndex);
+  });
+  server.on("/serverIndex", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", serverIndex);
+  });
+  /*handling uploading firmware file */
+  server.on("/update", HTTP_POST, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+    ESP.restart();
+  }, []() {
+    HTTPUpload& upload = server.upload();
+    if (upload.status == UPLOAD_FILE_START) {
+      Serial.printf("Update: %s\n", upload.filename.c_str());
+      if (!Update.begin(0xFFFFFFFF)) { //start with max available size
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_WRITE) {
+      /* flashing firmware to ESP*/
+      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_END) {
+      if (Update.end(true)) { //true to set the size to the current progress
+        Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+      } else {
+        Update.printError(Serial);
+      }
+    }
+  });
+  server.begin();
+
+   /* LED */
   pinMode(18 /*ESP32 pin GPIO18 connected to LED*/, 0x03);
 }
 
-void loop() {
-  ArduinoOTA.handle();
+void loop(void) {
+  server.handleClient();
+  delay(1);
 
   /* LED Blink */
   digitalWrite(18 /*ESP32 pin GPIO18 connected to LED*/, ledState);
---
+++ ESP32 DevKitC V4/build/sketch/README.md
@@ -0,0 +1,2 @@
+#line 1 "d:\\ESP32\\ESP32 DevKitC V4\\T06. OTA Web Updater DFU Src\\README.md"
+init
