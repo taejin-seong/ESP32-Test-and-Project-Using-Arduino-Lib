c407a6b1daa6d13521f6d48d837580185ce7face
taejin-seong <sungtj0130@naver.com>
2022-05-27 02:36:11 +0900

    init

--- ESP32 DevKitC V4/build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ ESP32 DevKitC V4/build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -1,26 +1,35 @@
-# 1 "d:\\ESP32\\ESP32 DevKitC V4\\T10. ESP32 BLE Slave\\BLE_Serial_Slave_Test.ino"
-//This example code is in the Public Domain (or CC0 licensed, at your option.)
-//By Evandro Copercini - 2018
-//
-//This example creates a bridge between Serial and Classical Bluetooth (SPP)
-//and also demonstrate that SerialBT have the same functionalities of a normal Serial
-
-# 8 "d:\\ESP32\\ESP32 DevKitC V4\\T10. ESP32 BLE Slave\\BLE_Serial_Slave_Test.ino" 2
-# 17 "d:\\ESP32\\ESP32 DevKitC V4\\T10. ESP32 BLE Slave\\BLE_Serial_Slave_Test.ino"
-BluetoothSerial SerialBT;
+# 1 "d:\\ESP32\\ESP32 DevKitC V4\\T11. Joystick\\Joystick_Test.ino"
+const int X_PIN = 27; // X 축 아날로그 입력 핀, ESP32 pin GIOP27 (ADC2_7) connected to Joystick 
+const int Y_PIN = 26; // Y 축 아날로그 입력 핀, ESP32 pin GIOP26 (ADC2_9) connected to Joystick 
+const int S_Pin = 25; // Select 디지털 입력 핀, ESP32 pin GIOP32 connected to Joystick 
 
 void setup() {
+  // 모니터용 시리얼 시작
   Serial.begin(115200);
-  SerialBT.begin("ESP32test"); //Bluetooth device name
-  Serial.println("The device started, now you can pair it with bluetooth!");
+  // Select 디지털 입력핀 설정, 풀업 상태로 설정.
+  pinMode(S_Pin, 0x05);
 }
 
 void loop() {
-  if (Serial.available()) {
-    SerialBT.write(Serial.read());
-  }
-  if (SerialBT.available()) {
-    Serial.write(SerialBT.read());
-  }
-// delay(20);
+ // sel 핀 읽어오기
+ int sel = digitalRead(S_Pin);
+ // y 축 핀 아날로그 입력 읽어오기
+ int y = analogRead(Y_PIN);
+ // x 축 핀 아날로그 입력 읽어오기
+ int x = analogRead(X_PIN);
+
+//ESP32는 아날로그의 입력범위가 0~4095 (12bit 해상도)
+// 0-255로 변경 (12bit (0~4095)-> 8bit (0~255))
+ y >>=4;
+
+ // 출력!
+ Serial.print("x: ");
+ Serial.print(x);
+ Serial.print(", y: ");
+ Serial.print(y);
+ Serial.print(", push: ");
+ Serial.println(sel);
+
+ //100ms 대기
+ delay(100);
 }
---
+++ ESP32 DevKitC V4/build/sketch/Joystick_Test.ino.cpp
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+#line 1 "d:\\ESP32\\ESP32 DevKitC V4\\T11. Joystick\\Joystick_Test.ino"
+const int X_PIN   = 27; // X 축 아날로그 입력 핀, ESP32 pin GIOP27 (ADC2_7) connected to Joystick 
+const int Y_PIN   = 26; // Y 축 아날로그 입력 핀, ESP32 pin GIOP26 (ADC2_9) connected to Joystick 
+const int S_Pin   = 25; // Select 디지털 입력 핀, ESP32 pin GIOP32 connected to Joystick 
+
+#line 5 "d:\\ESP32\\ESP32 DevKitC V4\\T11. Joystick\\Joystick_Test.ino"
+void setup();
+#line 12 "d:\\ESP32\\ESP32 DevKitC V4\\T11. Joystick\\Joystick_Test.ino"
+void loop();
+#line 5 "d:\\ESP32\\ESP32 DevKitC V4\\T11. Joystick\\Joystick_Test.ino"
+void setup() {
+  // 모니터용 시리얼 시작
+  Serial.begin(115200);
+  // Select 디지털 입력핀 설정, 풀업 상태로 설정.
+  pinMode(S_Pin, INPUT_PULLUP);   
+}
+
+void loop() {
+ // sel 핀 읽어오기
+ int sel = digitalRead(S_Pin);
+ // y 축 핀 아날로그 입력 읽어오기
+ int y = analogRead(Y_PIN);
+ // x 축 핀 아날로그 입력 읽어오기
+ int x = analogRead(X_PIN);
+
+//ESP32는 아날로그의 입력범위가 0~4095 (12bit 해상도)
+// 0-255로 변경 (12bit (0~4095)-> 8bit (0~255))
+ y >>=4;
+
+ // 출력!
+ Serial.print("x: ");
+ Serial.print(x);
+ Serial.print(", y: ");
+ Serial.print(y);
+ Serial.print(", push: ");
+ Serial.println(sel);
+ 
+ //100ms 대기
+ delay(100);
+}
