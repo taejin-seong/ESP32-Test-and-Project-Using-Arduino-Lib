04e38c6fc0ce3daa74a1368a49e27182212c83a9
taejin-seong <sungtj0130@naver.com>
2022-06-10 02:12:49 +0900

    init

--- ESP32 DevKitC V4/build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ ESP32 DevKitC V4/build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -1,36 +1,51 @@
-# 1 "d:\\ESP32\\ESP32 DevKitC V4\\T11. Joystick\\Joystick_Test.ino"
-const int X_PIN = 27; // X 축 아날로그 입력 핀, ESP32 pin GIOP27 (ADC2_7) connected to Joystick 
-const int Y_PIN = 26; // Y 축 아날로그 입력 핀, ESP32 pin GIOP26 (ADC2_9) connected to Joystick 
-const int S_Pin = 25; // Select 디지털 입력 핀, ESP32 pin GIOP32 connected to Joystick 
+# 1 "d:\\ESP32\\ESP32 DevKitC V4\\T14. ESP32 MQTT Publish\\ESP32_MQTT_Publish_Test.ino"
+# 2 "d:\\ESP32\\ESP32 DevKitC V4\\T14. ESP32 MQTT Publish\\ESP32_MQTT_Publish_Test.ino" 2
+# 3 "d:\\ESP32\\ESP32 DevKitC V4\\T14. ESP32 MQTT Publish\\ESP32_MQTT_Publish_Test.ino" 2
+
+const char* ssid = "Home";
+const char* password = "257535411186f";
+const char* mqttServer = "broker.mqtt-dashboard.com";;
+const int mqttPort = 1883;
+//const char* mqttUser = "yourMQTTuser";
+//const char* mqttPassword = "yourMQTTpassword";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 void setup() {
-  // 모니터용 시리얼 시작
+
   Serial.begin(115200);
-  // Select 디지털 입력핀 설정, 풀업 상태로 설정.
-  pinMode(S_Pin, 0x05);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  Serial.println("Connected to the WiFi network");
+
+  client.setServer(mqttServer, mqttPort);
+
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+
+    if (client.connect("ESP32Client")) {
+
+      Serial.println("connected");
+
+    } else {
+
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+
+    }
+  }
+
+  client.publish("esp/test", "Hello, Subscriber! I'm ESP32 Publisher ");
+
 }
 
 void loop() {
- // sel 핀 읽어오기
- int sel = digitalRead(S_Pin);
- // y 축 핀 아날로그 입력 읽어오기
- int y = analogRead(Y_PIN);
- // x 축 핀 아날로그 입력 읽어오기
- int x = analogRead(X_PIN);
-
-//ESP32는 아날로그의 입력범위가 0~4095 (12bit 해상도)
-// 0-255로 변경 (12bit (0~4095)-> 8bit (0~255))
- x >>=4;
- y >>=4;
-
- // 출력!
- Serial.print("x: ");
- Serial.print(x);
- Serial.print(", y: ");
- Serial.print(y);
- Serial.print(", push: ");
- Serial.println(sel);
-
- //100ms 대기
- delay(100);
+  client.loop();
 }
